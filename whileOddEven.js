c6d56ccd4895c3555a083a485fa4349d69125700
faysalahmed93 <faysalahmed2811@gmail.com>
2024-02-04 10:28:53 +0600

    Odd Even  file added

---
+++ whileOddEven.js
@@ -0,0 +1,19 @@
+//--Find all the odd numbers from 61 to 100 --//
+let i = 61;
+while (i < 100) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+
+  i++;
+}
+
+// -- Find all the even numbers from 78 to 98 -- //
+
+console.log("This value is for even numbers :-");
+
+let k = 78;
+while (k <= 98) {
+  console.log(k);
+  k += 2;
+}
